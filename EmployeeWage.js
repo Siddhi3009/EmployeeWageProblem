83e5bd3b98c068e19e7a61847f5d4e3cace06bf0
Administrator <siddhiseth3009@gmail.com>
2020-11-18 21:54:10 +0530

    [Siddhi]Refactor. Emphrs rigid limit set

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,7 +4,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const TOTAL_WORKING_DAYS = 20;
-const MAXIMUM_WORKING_HOURS = 100;
+const MAXIMUM_WORKING_HOURS = 80;
 function GetWorkingHours(empCheck)
 { 
     switch(empCheck)
@@ -22,15 +22,14 @@ function GetWorkingHours(empCheck)
 }
 let empHrs = 0;
 let totalWorkingDays = 0;
-for(let day = 1; day<= TOTAL_WORKING_DAYS; day++)
+while(totalWorkingDays< TOTAL_WORKING_DAYS && empHrs< MAXIMUM_WORKING_HOURS)
 {
    let empCheck = Math.floor(Math.random() * 10) % 3;
    empHrs += GetWorkingHours(empCheck);
-   totalWorkingDays = day;
+   totalWorkingDays++;
+   //Consideration when empHrs rigid limit
    if(empHrs >= MAXIMUM_WORKING_HOURS)
-   {
-       break;
-   }
+    empHrs = MAXIMUM_WORKING_HOURS;
 }
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total working days: " + totalWorkingDays + "Total Working hours: "+ empHrs + "Employee Wage: " + empWage);
+console.log("Total working days: " + totalWorkingDays + " Total Working hours: "+ empHrs + " Employee Wage: " + empWage);
