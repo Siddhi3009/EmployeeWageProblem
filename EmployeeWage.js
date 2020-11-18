095906eb89f743f5deffb46c6a1576528a9a7385
Administrator <siddhiseth3009@gmail.com>
2020-11-18 23:15:24 +0530

    [Siddhi]Add. array foreach and reduce method added for total wage computation

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,15 +1,20 @@
+//constants
 const MAX_HRS_IN_MONTH=160;
 const NUM_OF_WORKING_DAYS=20;
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const WAGE_PER_HOUR=20;
+//variables
 let totalEmpHrs=0;
 let totalWorkingDays=0;
+//employee wage array
 let empDailyWageArr=new Array();
+//employee wage calculation using working hours
 function CalculateDailyWage(empHrs)
 {
   return empHrs*WAGE_PER_HOUR;
 }
+//get daily working hours 
 function GetWorkingHours(empCheck)
 {
     switch(empCheck)
@@ -22,6 +27,7 @@ function GetWorkingHours(empCheck)
             return 0;
    }
 }
+//Adding daily employee wage to an array
 while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
   totalWorkingDays++;
@@ -30,7 +36,21 @@ while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
   totalEmpHrs+=empHrs;
   empDailyWageArr.push(CalculateDailyWage(empHrs));
 }
- 
-let empWage = CalculateDailyWage(totalEmpHrs);
-console.log("Total Working Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+ empWage);
+//Total wage calculation
+let empWage=CalculateDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+empWage);
+ //Total employee wage calculation using array foreach
+let totalEmpWage=0;
+function sum(dailyWage)
+{
+  totalEmpWage+=dailyWage;
+}
+empDailyWageArr.forEach(sum);
+console.log("UC7A Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + totalEmpWage);
+//Total wage using reduce method of array
+function totalWages(totalWage, dailyWage)
+{
+  return totalWage+dailyWage;
+}
+console.log("UC7A Emp Wage with reduce: " + empDailyWageArr.reduce(totalWages, 0));
  
