3bd6cd5bb65cdb4a0201d89086c2c6d5bd81f65d
Administrator <siddhiseth3009@gmail.com>
2020-11-18 22:28:07 +0530

    [Siddhi]Add. Daily wage array added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,35 +1,36 @@
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-const TOTAL_WORKING_DAYS = 20;
-const MAXIMUM_WORKING_HOURS = 80;
+const MAX_HRS_IN_MONTH=160;
+const NUM_OF_WORKING_DAYS=20;
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const WAGE_PER_HOUR=20;
+let totalEmpHrs=0;
+let totalWorkingDays=0;
+let empDailyWageArr=new Array();
+function CalculateDailyWage(empHrs)
+{
+  return empHrs*WAGE_PER_HOUR;
+}
 function GetWorkingHours(empCheck)
-{ 
+{
     switch(empCheck)
     {
-        case IS_PART_TIME:
-        return PART_TIME_HOURS;
-        break;
-        case IS_FULL_TIME:
-        return FULL_TIME_HOURS;
-        break;
-        default:
-        return 0;
-        break;
-    }
+       case IS_PART_TIME:
+            return 4;
+       case IS_FULL_TIME:
+            return 8;
+       default:
+            return 0;
+   }
 }
-let empHrs = 0;
-let totalWorkingDays = 0;
-while(totalWorkingDays< TOTAL_WORKING_DAYS && empHrs< MAXIMUM_WORKING_HOURS)
+while(totalEmpHrs <=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
-   let empCheck = Math.floor(Math.random() * 10) % 3;
-   empHrs += GetWorkingHours(empCheck);
-   totalWorkingDays++;
-   //Consideration when empHrs rigid limit
-   if(empHrs >= MAXIMUM_WORKING_HOURS)
-    empHrs = MAXIMUM_WORKING_HOURS;
+  totalWorkingDays++;
+  let empCheck = Math.floor(Math.random()*10)%3;
+  let empHrs = GetWorkingHours(empCheck);
+  totalEmpHrs+=empHrs;
+  empDailyWageArr.push(CalculateDailyWage(empHrs));
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total working days: " + totalWorkingDays + " Total Working hours: "+ empHrs + " Employee Wage: " + empWage);
+ 
+let empWage = CalculateDailyWage(totalEmpHrs);
+console.log("Total Working Days: "+totalWorkingDays+" Total Hrs: "+totalEmpHrs+" Emp Wage: "+ empWage);
+ 
